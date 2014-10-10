8e4851caae6cf7a9ae1e6e201317fe7657ee9cef
Peter StJ <regardingscot@gmail.com>
2014-10-10 15:09:59 +0300

    Include the ripple element in the demo.

--- js/matetial.js
+++ js/matetial.js
@@ -2,13 +2,15 @@ goog.provide('material.app');
 
 goog.require('pstj.material.DrawerPanel');
 goog.require('pstj.material.HeaderPanel');
+goog.require('pstj.material.Ripple');
+
 
 
 var panel = new pstj.material.HeaderPanel();
 var panel2 = new pstj.material.DrawerPanel();
+var ripple = new pstj.material.Ripple();
+ripple.setRecenterRipples(true);
 panel2.render();
-setTimeout(function() {
-  console.log('Adding the child now');
-  panel2.addToMain(panel, true);
-}, 2000);
+panel2.addToMain(panel, true);
+panel.addToMain(ripple, true);
 
